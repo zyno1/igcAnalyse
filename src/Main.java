7616dd58fe18dff2aaaf673cd4095f04569db98c
zyno1 <olivierzeyen@gmail.com>
2019-08-07 10:17:41 +0200

    cli added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import lib.igc.Flight;
 import lib.igc.FlightCollection;
-import lib.igc.Point;
-import lib.igc.StandardizePair;
-import lib.obj.PointCollection;
 import lib.thermals.ThermalCollection;
 import lib.wind.Entry;
 import lib.wind.Wind;
@@ -13,136 +10,215 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Stack;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static void main(String[] argc) {
-        /*try {
-            FlightCollection tmp = new FlightCollection("data/NetCoupe2019_8643.igc", "data/NetCoupe2019_8606.igc");
-            //FlightCollection tmp = new FlightCollection("data/2019-06-01-XCS-AAA-01.igc", "data/2019-06-01-XCS-AAA-02.igc");
-            //Flight tmp = new Flight("data/NetCoupe2019_8643.igc");
-            //Flight tmp = new Flight("data/2019-06-01-XCS-AAA-02.igc");
-            ArrayList<Point> th = tmp.findThermals();
-            ThermalCollection thc = new ThermalCollection();
-            thc.addThermals(th.toArray(new Point[0]));
-            thc.sort();
 
-            StandardizePair sdp = tmp.standardize();
-            thc.standardize(sdp);
+    private static int THREAD_COUNT = 1;
+    private static String FOLDER = "data/";;
+    private static int MAX_COUNT = -1;
+    private static int MIN = -1;
 
-            PointCollection pc = new PointCollection();
-            pc.addFlightCollection(tmp);
-            pc.writeToFile("data.obj");
+    private static String WIND_DB = null;
+    private static String WIND_DATE = null;
 
-            pc = new PointCollection();
-            pc.addThermalCollection(thc);
-            pc.writeToFile("thermals.obj");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+    private static String LOCATION = null;
 
-        try {
-            Wind w = new Wind("data/wind.txt");
-            w.matchLocation("LFNC");
-            /*Entry[] date = w.get("LFNC", "2019063015");
 
-            if(date.length != 0) {
-                for(Entry tmp : date) {
-                    w.matchDirection(tmp.getAltitude(), tmp.getDirection(), 5);
-                    w.matchSpeed(tmp.getAltitude(), tmp.getSpeed(), 20);
+    private static void loadOptions(String[] arg) {
+        for(int i = 0; i < arg.length; i++) {
+            String tmp = arg[i];
+
+            if(tmp.equals("-h") || tmp.equals("--help")) {
+                StringBuilder str = new StringBuilder();
+
+                str.append("-h\n");
+                str.append("--help             display help screen\n");
+                str.append("--threads <n>      set the number of threads to use\n");
+                str.append("-i <s>             set the path that contains the flight logs\n");
+                str.append("--location <s>     set the location to use\n");
+                str.append("--max-count <n>    set the maximum number of thermals to keep\n");
+                str.append("--min <n>          set the minimum amount of times a thermal has been found to be kept\n");
+                str.append("--wind-db <s>      set the path to the wind database\n");
+                str.append("--wind-date <s>     search all the dates in the database that are similar to this one\n");
+
+                System.out.println(str.toString());
+                System.exit(0);
+            }
+            else if(tmp.equals("--wind-db")) {
+                WIND_DB = arg[++i];
+            }
+            else if(tmp.equals("--wind-date")) {
+                WIND_DATE = arg[++i];
+            }
+            else if(tmp.equals("--location")) {
+                LOCATION = arg[++i];
+            }
+            else if(tmp.equals("--threads")) {
+                THREAD_COUNT = Integer.parseInt(arg[++i]);
+            }
+            else if(tmp.equals("--max-count")) {
+                MAX_COUNT = Integer.parseInt(arg[++i]);
+            }
+            else if(tmp.equals("--min")) {
+                MIN = Integer.parseInt(arg[++i]);
+            }
+            else if(tmp.equals("-i")) {
+                FOLDER = arg[++i];
+            }
+        }
+    }
+
+    private static ArrayList<String> findAllFiles(String path) {
+        ArrayList<String> res = new ArrayList<>();
+
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        if(listOfFiles != null) {
+            for (File file : listOfFiles) {
+                if (file.isFile() && file.getAbsoluteFile().toString().contains(".igc")) {
+                    res.add(file.getAbsolutePath());
                 }
-            }*/
+                else if(file.isDirectory()) {
+                    res.addAll(findAllFiles(file.getAbsolutePath()));
+                }
+            }
+        }
+        else {
+            System.out.println(path + " : not found");
+        }
 
-            System.out.println("Dates;");
-            System.out.println("\t" + Arrays.toString(w.getDates()));
-            System.out.println("---------------------------");
+        return res;
+    }
 
-            FlightCollection fc = new FlightCollection();
-            for(String d : w.getDates()) {
-                String path = "data/LFNC/" + d + "/";
-                //System.out.println(path);
+    private static ArrayList<String> findMatchingOnly(String path, String location, String[] dates) {
+        ArrayList<String> res = new ArrayList<>();
 
-                File folder = new File(path);
-                File[] listOfFiles = folder.listFiles();
+        for(String d : dates) {
+            String tmp = path + "/" + location + "/" + d + "/";
+            //System.out.println(path);
 
-                if(listOfFiles != null) {
-                    for (File file : listOfFiles) {
-                        if (file.isFile()) {
-                            //System.out.println(file.getAbsolutePath());
-                            fc.addFlight(new Flight(file.getAbsolutePath()));
-                        }
+            File folder = new File(tmp);
+            File[] listOfFiles = folder.listFiles();
+
+            if(listOfFiles != null) {
+                for (File file : listOfFiles) {
+                    if (file.isFile()) {
+                        //System.out.println(file.getAbsolutePath());
+                        //fc.addFlight(new Flight(file.getAbsolutePath()));
+                        res.add(file.getAbsolutePath());
                     }
                 }
-                else {
-                    System.out.println(path + " : not found");
+            }
+            else {
+                System.out.println(path + " : not found");
+            }
+        }
+
+        return res;
+    }
+
+    private static void operate() {
+        ArrayList<String> igcPaths = null;
+
+        if(WIND_DB != null && WIND_DATE != null) {
+            try {
+                Wind w = new Wind(WIND_DB);
+                w.matchLocation(LOCATION);
+
+                Entry[] date = w.get(LOCATION, WIND_DATE);
+
+                if(date.length != 0) {
+                    for(Entry tmp : date) {
+                        w.matchDirection(tmp.getAltitude(), tmp.getDirection(), 5);
+                        w.matchSpeed(tmp.getAltitude(), tmp.getSpeed(), 20);
+                    }
+                }
+
+                igcPaths = findMatchingOnly(FOLDER, LOCATION, w.getDates());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            igcPaths = findAllFiles(FOLDER);
+        }
+
+        if(igcPaths == null) {
+            System.err.println("Could find any igc files");
+            System.exit(1);
+        }
+
+        ThermalCollection tc = new ThermalCollection();
+
+        AtomicInteger co = new AtomicInteger(0);
+        Iterator<String> it = igcPaths.iterator();
+        final int size = igcPaths.size();
+
+        Runnable r = () -> {
+            String f = null;
+            synchronized (it) {
+                if(it.hasNext()) {
+                    f = it.next();
                 }
             }
 
-            int i = 0;
-
-            ThermalCollection tc = new ThermalCollection();
-            Iterator<Flight> it = fc.iterator();
-
-            AtomicInteger co = new AtomicInteger(0);
-
-            Runnable r = () -> {
-                Flight f = null;
-                synchronized (it) {
-                    if(it.hasNext()) {
-                        f = it.next();
-                    }
-                }
-
-                while (f != null) {
-                    for(Flight t : f.findThermals()) {
+            while (f != null) {
+                try {
+                    for(Flight t : (new Flight(f)).findThermals()) {
                         synchronized (tc) {
                             tc.addThermal(t.averagePos());
                         }
                     }
-                    System.out.println(co.getAndIncrement() + "/" + fc.size());
-                    synchronized (it) {
-                        if(it.hasNext()) {
-                            f = it.next();
-                        }
-                        else {
-                            f = null;
-                        }
+                } catch (IOException e) {
+                    System.out.println("failed to load file: " + f);
+                }
+                System.out.println(co.getAndIncrement() + "/" + size);
+                synchronized (it) {
+                    if(it.hasNext()) {
+                        f = it.next();
+                    }
+                    else {
+                        f = null;
                     }
                 }
-            };
-
-            Thread[] threads = new Thread[3];
-
-            for (int k = 0; k < threads.length; k++) {
-                threads[k] = new Thread(r);
-                threads[k].start();
             }
+        };
 
-            r.run();
+        Thread[] threads = null;
+        if(THREAD_COUNT > 1) {
+            threads = new Thread[THREAD_COUNT - 1];
 
-            for(Thread t : threads) {
+            for(int i = 0; i < threads.length; i++) {
+                threads[i] = new Thread(r);
+                threads[i].start();
+            }
+        }
+
+        r.run();
+
+        if(threads != null) {
+            for (Thread t : threads) {
                 try {
                     t.join();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            /*for(Flight f : fc) {
-                for(Flight t : f.findThermals()) {
-                    tc.addThermal(t.averagePos());
-                }
-                System.out.println(i++ + "/" + fc.size());
-            }*/
+        }
 
-            i = 5;
-            while (tc.size() > 150) {
-                tc.filter(i++);
-            }
+        int i = 1;
+        if(i < MIN) {
+            i = MIN;
+            tc.filter(i);
+        }
 
-            //System.out.println(tc.toCUP());
+        while (tc.size() > MAX_COUNT || MAX_COUNT == -1) {
+            tc.filter(i++);
+        }
+
+        try {
             FileWriter res = new FileWriter("res.cup");
             res.write(tc.toCUP());
             res.flush();
@@ -150,5 +226,10 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] arg) {
+        loadOptions(arg);
+        operate();
     }
 }
