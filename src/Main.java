fe3236ad69438a35db9de1db4bf9aeb9855d13a3
zyno1 <olivierzeyen@gmail.com>
2020-02-21 10:24:35 +0100

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,6 @@ import lib.dao.thermals.ThermalCollectionDAO;
 import lib.dao.thermals.ThermalCollectionKML;
 import lib.igc.Flight;
 import lib.thermals.ThermalCollection;
-import lib.wind.Entry;
-import lib.wind.Wind;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,11 +35,6 @@ public class Main {
     private static int MAX_COUNT = -1;
     private static int MIN = -1;
 
-    private static String WIND_DB = null;
-    private static String WIND_DATE = null;
-
-    private static String LOCATION = null;
-
 
     private static void loadOptions(String[] arg) {
         for(int i = 0; i < arg.length; i++) {
@@ -54,23 +47,11 @@ public class Main {
                 str.append("--help             display help screen\n");
                 str.append("--threads <n>      set the number of threads to use\n");
                 str.append("-i <s>             set the path that contains the flight logs\n");
-                str.append("--location <s>     set the location to use\n");
                 str.append("--max-count <n>    set the maximum number of thermals to keep\n");
                 str.append("--min <n>          set the minimum amount of times a thermal has been found to be kept\n");
-                str.append("--wind-db <s>      set the path to the wind database\n");
-                str.append("--wind-date <s>     search all the dates in the database that are similar to this one\n");
 
                 System.out.println(str.toString());
                 System.exit(0);
-            }
-            else if(tmp.equals("--wind-db")) {
-                WIND_DB = arg[++i];
-            }
-            else if(tmp.equals("--wind-date")) {
-                WIND_DATE = arg[++i];
-            }
-            else if(tmp.equals("--location")) {
-                LOCATION = arg[++i];
             }
             else if(tmp.equals("--threads")) {
                 THREAD_COUNT = Integer.parseInt(arg[++i]);
@@ -110,60 +91,11 @@ public class Main {
         return res;
     }
 
-    private static ArrayList<String> findMatchingOnly(String path, String location, String[] dates) {
-        ArrayList<String> res = new ArrayList<>();
-
-        for(String d : dates) {
-            String tmp = path + "/" + location + "/" + d + "/";
-            //System.out.println(path);
-
-            File folder = new File(tmp);
-            File[] listOfFiles = folder.listFiles();
-
-            if(listOfFiles != null) {
-                for (File file : listOfFiles) {
-                    if (file.isFile()) {
-                        //System.out.println(file.getAbsolutePath());
-                        //fc.addFlight(new Flight(file.getAbsolutePath()));
-                        res.add(file.getAbsolutePath());
-                    }
-                }
-            }
-            else {
-                System.out.println(path + " : not found");
-            }
-        }
-
-        return res;
-    }
-
     private static void operate() {
-        ArrayList<String> igcPaths = null;
+        ArrayList<String> igcPaths = findAllFiles(FOLDER);
 
-        if(WIND_DB != null && WIND_DATE != null) {
-            try {
-                Wind w = new Wind(WIND_DB);
-                w.matchLocation(LOCATION);
 
-                Entry[] date = w.get(LOCATION, WIND_DATE);
-
-                if(date.length != 0) {
-                    for(Entry tmp : date) {
-                        w.matchDirection(tmp.getAltitude(), tmp.getDirection(), 5);
-                        w.matchSpeed(tmp.getAltitude(), tmp.getSpeed(), 20);
-                    }
-                }
-
-                igcPaths = findMatchingOnly(FOLDER, LOCATION, w.getDates());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            igcPaths = findAllFiles(FOLDER);
-        }
-
-        if(igcPaths == null) {
+        if(igcPaths.size() == 0) {
             System.err.println("Could find any igc files");
             System.exit(1);
         }
